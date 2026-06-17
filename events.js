32863fa74bd2d2d149243fad31c78b266bd15ef7
M. Jones <miriam.j.jones.work@gmail.com>
2026-06-17 13:49:43 -0500

    6.17.26 1.49pm Finished the mario movement.

--- events.js
+++ events.js
@@ -10,57 +10,71 @@ let changeColorButton = document.querySelector(".change-color");
 let endGameButton = document.querySelector(".end-game");
 let updateScoreButton = document.querySelector(".update-score");
 let growButton = document.querySelector(".grow");
+let forwardButton = document.querySelector(".forward");
+let backwardButton = document.querySelector(".backward");
+let x=0;
 
-/* 1. When jump button is clicked, Mario jumps and coin disappears.  
+// 1. When jump button is clicked, Mario jumps and coin disappears.  
 
-________.addEventListener("click", function() {
+jumpButton.addEventListener("click", function() {
     mario.style.bottom = "120px";
+    mario.style.transition = "bottom .3s";
     coin.style.display = "none";
 });
 
-*/
 
+// 2. When jump down button is clicked, Mario jumps down. 
 
-/* 2. When jump down button is clicked, Mario jumps down. 
-
-________.addEventListener("click", function() {
-    mario.style = "bottom: 35px";
+jumpDownButton.addEventListener("click", function() {
+    mario.style.transition = "bottom .3s";
+    mario.style.bottom = "35px";
 });
 
-*/
 
+forwardButton.addEventListener("click", function() {
+    x+=1;
+    mario.style.transition = "left .3s";
+    coin.style.transition = "left .3s";
+    mario.style.left= "calc(".concat((getComputedStyle(mario)).left," + 120px - 35px)");
+    coin.style.left = "calc(".concat((getComputedStyle(coin)).left," + 120px - 35px)");
+});
 
-/* 3. When change color button is clicked, background color changes. 
+backwardButton.addEventListener("click", function() {
+    x-=1;
+    mario.style.transition = "left .3s";
+    coin.style.transition = "left .3s";
+    mario.style.left= "calc(".concat((getComputedStyle(mario)).left," - 120px + 35px)");
+    coin.style.left = "calc(".concat((getComputedStyle(coin)).left," - 120px + 35px)");
+});
 
-________.addEventListener("click", function() {
+// 3. When change color button is clicked, background color changes. 
+
+changeColorButton.addEventListener("click", function() {
     background.style.backgroundColor = "springGreen"; // you can change the color to something different
 });
 
-*/
 
 
-/* 4. When end game button is clicked, the title of the page says game over. 
+// 4. When end game button is clicked, the title of the page says game over. 
 
-________._______ ("___", ________() {
-    title.innerHTML = "Game over.";
+endGameButton.addEventListener("click", function() {
+    title.innerHTML = 'Game ${x} over.';
 });
 
-*/
 
 
-/* 5. When update score button is clicked, the counter score becomes "1" instead of 0. 
+// 5. When update score button is clicked, the counter score becomes "1" instead of 0. 
 
-________._______ ("___", ________() {
+updateScoreButton.addEventListener("click", function() {
     score.innerHTML = 1;
 });
 
-*/
 
 
-/* 6: When clicking the "Grow" button the Mario image gets bigger. Write the whole click handler! 
+// 6: When clicking the "Grow" button the Mario image gets bigger. Write the whole click handler! 
+growButton.addEventListener("click", function(){
+    mario.style.width = "150px";
+    mario.style.height = "150px";
+    mario.style.transition = "height width .3s";
+});
 
-{
-    mario.style.width = "200px";
-}
-
-*/
